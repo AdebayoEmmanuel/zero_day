12585784126a46e69e4ca4bde54c1e969c27d79a
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-02 00:10:44 +0000

    TODAY I learned looping

--- 0x01-learn_py/0-print_odds.py
+++ 0x01-learn_py/0-print_odds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # loop through range 0-20 using for loop
-for i in range(21):
+for i in range(1,21):
 # check for odd using modulus and if statement
     if i % 2 != 0:
 # print odd
